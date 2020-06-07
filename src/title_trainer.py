9cd547c35c6f0045306763f758e2e6d0c8a09ed9
flicknewb <theowenmorris@gmail.com>
2020-06-06 20:41:53 -0700

    aws text trainer updates

--- src/title_trainer.py
+++ src/title_trainer.py
@@ -84,6 +84,41 @@ def on_epoch_end(epoch, _):
         pass
     pass
 
+def spit_out_text():
+    # Function invoked at end of each epoch. Prints generated text.
+    print("****************************************************************************")
+    #print('----- Generating text after Epoch: %d' % epoch)
+    
+    start_index = random.randint(0, len(processed_text) - maxlen - 1)
+    for temperature in [ 1.0]:
+        print('----- temperature:', temperature)
+
+        generated = ''
+        sentence = processed_text[start_index: start_index + maxlen]
+        #sentence = 'the matrix  |  the fifth element  |  han'
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+
+        for i in range(1000):
+            x_pred = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x_pred[0, t, char_indices[char]] = 1.
+
+            preds = model.predict(x_pred, verbose=0)[0]
+            next_index = sample(preds, temperature)
+            next_char = indices_char[next_index]
+
+            generated += next_char
+            sentence = sentence[1:] + next_char
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        with open('../data/raw_generated_text_local.txt', "a+") as f:
+              f.write(generated[maxlen:]+"\n")
+        print()
+
+# ***************** EVERYTHING BELOW HERE RUNS AFTER ALL FUNCTIONS AND DATA HAVE BEEN LOADED ****************
 
 processed_text = title_string.lower()
 processed_text = re.sub(r'[^\x00-\x7f]',r'', processed_text)
@@ -93,7 +128,7 @@ char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
 # cut the text in semi-redundant sequences of maxlen characters
-maxlen = 40
+maxlen = 30
 step = 3
 sentences = []
 next_chars = []
@@ -126,7 +161,7 @@ print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 print("fitting model")
 model.fit(x, y,
         batch_size=128,
-        epochs=60,
+        epochs=10,
         callbacks=[print_callback])
 
 #pickle.dump(model, open('../data/text_gen_model.pkl', "wb"))

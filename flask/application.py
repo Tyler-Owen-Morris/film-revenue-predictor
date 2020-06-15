c613364dee87bab20294e1c3a5b4b81d5371f62d
flicknewb <theowenmorris@gmail.com>
2020-06-14 23:30:02 -0700

    flask updates

--- flask/application.py
+++ flask/application.py
@@ -6,10 +6,12 @@ from wtforms import TextField, Form, SubmitField
 application = app = Flask(__name__)
 import time
 from fake_film_predictor import get_new_film_prediction
-from fake_film_predictor import get_gold_film_prediction
+from fake_film_predictor import get_gold_film_prediction, get_custom_film_prediciton
 
 actors = pd.read_csv('../data/actor_key.csv', index_col=0).name.values.tolist()
 directors = pd.read_csv('../data/all_directors.csv', index_col=0)['0'].values.tolist()
+productions = pd.read_csv('data/all_production.csv', index_col=0)['0'].values.tolist()
+distributors = pd.read_csv('data/all_distribution.csv', index_col=0)['0'].values.tolist()[1:]
 
 class SearchForm(Form):
     autocomp = TextField('Actor name', id='actor_autocomplete')
@@ -32,22 +34,38 @@ def details():
 def custom():
     #print(actors)
     form = SearchForm(request.form)
+    budgets = [10000,100000,1000000,100000000,200000000]
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 
+         'November', 'December']
     if request.method == 'POST':
         first_actor = str(request.form['input1'])
         second_actor = str(request.form['input2'])
         third_actor = str(request.form['input3'])
         fourth_actor = str(request.form['input4'])
         fifth_actor = str(request.form['input5'])
+        sixth_actor = str(request.form['input6'])
+        seventh_actor = str(request.form['input7'])
+        eighth_actor = str(request.form['input6'])
+        ninth_actor = str(request.form['input6'])
+        tenth_actor = str(request.form['input6'])
         director = str(request.form.get('inputD'))
         genres = request.form.getlist('genreCheckbox')
-        actor = [first_actor, second_actor, third_actor, fourth_actor, fifth_actor]
+        actors = [first_actor, second_actor, third_actor, fourth_actor, fifth_actor, sixth_actor, seventh_actor, eighth_actor, ninth_actor, tenth_actor]
         rating = request.form.get('ratingCheckbox')
-        print(director)
-        print(actor)
+        budget = request.form.get('budget')
+        prod = request.form.get('production')
+        dist = request.form.get('distribution')
+        month = request.form.get('month')
+        # print(director)
+        # print(actor)
         print(genres)
         print(rating)
-        
-    return render_template('custom.html', form=form)
+        # print(budget)
+        # print(prod,dist)
+        data = get_custom_film_prediciton(director, actors, genres, rating, budget, prod, dist, month)
+        return render_template('details.html', data=data)
+    
+    return render_template('custom.html', form=form, months=months, budgets=budgets, productions=productions, distributors=distributors)
 
 #auto complete feature for actors
 @app.route('/_autocomplete', methods=['GET'])
--- flask/fake_film_predictor.py
+++ flask/fake_film_predictor.py
@@ -350,6 +350,159 @@ def get_gold_film_prediction():
     r['rating'] = rate
     return r
 
+def get_custom_film_prediciton(director, actors, genres, rating, budget, production, distribution, month):
+    producer = producers.sample(1).values[0,0]
+    executive = executives.sample(1).values[0,0]
+    title = titles.sample(1).values[0,0]
+
+    film = pd.DataFrame(columns=['actor_1', 'actor_2', 'actor_3', 'actor_4', 'actor_5', 'actor_6', 'actor_7', 'actor_8', 
+                                'actor_9','actor_10', 'director', 'production', 'distribution', 'producer', 'executive', 
+                                'budget', 'genre', 'release_month', 'rating'])
+    film.loc[0] = [actors[0], actors[1], actors[2], actors[3], actors[4], actors[5], actors[6]
+                , actors[7], actors[8], actors[9], director, production, distribution, producer, 
+                executive, int(budget), genres, month, rating]
+
+    film['actor1'] = film['actor_1'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor2'] = film['actor_2'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor3'] = film['actor_3'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor4'] = film['actor_4'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor5'] = film['actor_5'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor6'] = film['actor_6'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor7'] = film['actor_7'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor8'] = film['actor_8'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor9'] = film['actor_9'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    film['actor10'] = film['actor_10'].map(lambda x : get_actor_key(x).replace(" ", '') if isinstance(x, str) else None)
+    print("first half actors constructed")
+    film['actor_1_string'] = film['actor1'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_2_string'] = film['actor2'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_3_string'] = film['actor3'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_4_string'] = film['actor4'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_5_string'] = film['actor5'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_6_string'] = film['actor6'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_7_string'] = film['actor7'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_8_string'] = film['actor8'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_9_string'] = film['actor9'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    film['actor_10_string'] = film['actor10'].map(lambda x : get_actor_name(x) if isinstance(x, str) else None)
+    print("actors constructed")
+    #time.sleep(0.25)
+    film['train_string'] = film[['production','distribution','director','actor_1','actor_2','actor_3','actor_4',
+                               'actor_5','actor_6','actor_7','actor_8','actor_9','actor_10',
+                               'producer','executive']].apply(lambda x: ' '.join(x.dropna().astype(str)), axis=1)
+
+    #we're going to do the OHE manually
+    film['action'] = film['genre'].map(lambda x : 1 if 'Action' in x else 0)
+    film['adventure']  = film['genre'].map(lambda x : 1 if 'Adventure' in x else 0)
+    film['animated'] = film['genre'].map(lambda x : 1 if 'Animation' in x else 0)
+    film['biography'] = film['genre'].map(lambda x : 1 if 'Biography' in x else 0)
+    film['drama'] = film['genre'].map(lambda x : 1 if 'Drama' in x else 0)
+    film['documentary'] = film['genre'].map(lambda x : 1 if 'Documentary' in x else 0)
+    film['comedy'] = film['genre'].map(lambda x : 1 if 'Comedy' in x else 0)
+    film['crime'] = film['genre'].map(lambda x : 1 if 'Crime' in x else 0)
+    film['fantasy'] = film['genre'].map(lambda x : 1 if 'Fantasy' in x else 0)
+    film['family'] = film['genre'].map(lambda x : 1 if 'Family' in x else 0)
+    film['musical'] = film['genre'].map(lambda x : 1 if 'Musical' in x else 0)
+    film['horror'] = film['genre'].map(lambda x : 1 if 'Horror' in x else 0)
+    film['war'] = film['genre'].map(lambda x : 1 if 'War' in x else 0)
+    film['mystery'] = film['genre'].map(lambda x : 1 if 'Mystery' in x else 0)
+    film['sci-fi'] = film['genre'].map(lambda x : 1 if 'Sci-Fi' in x else 0)
+    film['thriller'] = film['genre'].map(lambda x : 1 if 'Thriller' in x else 0)
+    film['romance'] = film['genre'].map(lambda x : 1 if 'Romance' in x else 0)
+
+    fake_popularity = 10071118
+    film['actor1_popularity'] = film['actor1'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor2_popularity'] = film['actor2'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor3_popularity'] = film['actor3'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor4_popularity'] = film['actor4'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor5_popularity'] = film['actor5'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor6_popularity'] = film['actor6'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor7_popularity'] = film['actor7'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor8_popularity'] = film['actor8'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor9_popularity'] = film['actor9'].map(lambda x : get_act_pop(x,fake_popularity))
+    film['actor10_popularity'] = film['actor10'].map(lambda x : get_act_pop(x,fake_popularity))
+
+    #convert popularity scores to text columns 
+    film['actor1_class'] = film['actor1_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor2_class'] = film['actor2_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor3_class'] = film['actor3_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor4_class'] = film['actor4_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor5_class'] = film['actor5_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor6_class'] = film['actor6_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor7_class'] = film['actor7_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor8_class'] = film['actor8_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor9_class'] = film['actor9_popularity'].map(lambda x : get_celeb_class(x))
+    film['actor10_class'] = film['actor10_popularity'].map(lambda x : get_celeb_class(x))
+
+    #reload the variable
+    dummy_blank = pd.read_csv('data/dummy_blank.csv', index_col=0)
+    dummy_blank.iloc[0, dummy_blank.columns.get_loc('release_month_'+film['release_month'][0]) ] = 1
+    dummy_blank.iloc[0, dummy_blank.columns.get_loc('actor1_class_'+film['actor1_class'][0]) ] = 1
+    dummy_blank.iloc[0, dummy_blank.columns.get_loc('actor2_class_'+film['actor2_class'][0]) ] = 1
+    dummy_blank.iloc[0, dummy_blank.columns.get_loc('actor3_class_'+film['actor3_class'][0]) ] = 1
+    print (film['rating'][0])
+    if film['rating'][0] == 'not-rated' or film['rating'][0] == '':
+        dummy_blank.iloc[0, dummy_blank.columns.get_loc('rating_'+'Not Rated') ] = 1
+    else:
+        try:
+            print(dummy_blank.columns.get_loc('rating_'+film['rating'][0]))
+            dummy_blank.iloc[0, dummy_blank.columns.get_loc('rating_'+film['rating'][0]) ] = 1
+        except:
+            print ("failed ")
+
+    vectorizer = HashingVectorizer(n_features=1000)
+    vector = vectorizer.fit_transform(film['train_string'].to_numpy())
+    vec_df = pd.DataFrame.sparse.from_spmatrix(vector)
+    use_cols = film[['budget','action','adventure','animated','biography','drama','documentary','comedy','crime',
+                        'fantasy','family','musical','horror','war','mystery','sci-fi','thriller','romance']]
+    X = pd.concat([use_cols, dummy_blank, vec_df], axis=1, sort=False)
+
+    prediction = model_gb.predict(X)
+    
+    #construct return dict with prediction
+    r = {}
+    r['predicted_revenue'] = "{:,}".format(round(prediction[0],0))
+    r['title'] = title.title()
+    r['director'] = director
+    r['producer'] = producer
+    r['executive'] = executive
+    r['release_month'] = month
+    print("this is the budget: "+ budget)
+    r['budget'] = "{:,}".format(int(film['budget']))
+    r['actor1'] = film.actor_1_string[0]
+    r['actor1_class'] = film.actor1_class[0]
+    r['actor1_code'] = film.actor1[0]
+    r['actor2'] = film.actor_2_string[0]
+    r['actor2_class'] = film.actor2_class[0]
+    r['actor2_code'] = film.actor2[0]
+    r['actor3'] = film.actor_3_string[0]
+    r['actor3_class'] = film.actor3_class[0]
+    r['actor3_code'] = film.actor3[0]
+    r['actor4'] = film.actor_4_string[0]
+    r['actor4_class'] = film.actor4_class[0]
+    r['actor4_code'] = film.actor4[0]
+    r['actor5'] = film.actor_5_string[0]
+    r['actor5_class'] = film.actor5_class[0]
+    r['actor5_code'] = film.actor5[0]
+    r['actor6'] = film.actor_6_string[0]
+    r['actor6_class'] = film.actor6_class[0]
+    r['actor6_code'] = film.actor6[0]
+    r['actor7'] = film.actor_7_string[0]
+    r['actor7_class'] = film.actor7_class[0]
+    r['actor7_code'] = film.actor7[0]
+    r['actor8'] = film.actor_8_string[0]
+    r['actor8_class'] = film.actor8_class[0]
+    r['actor8_code'] = film.actor8[0]
+    r['actor9'] = film.actor_9_string[0]
+    r['actor9_class'] = film.actor9_class[0]
+    r['actor9_code'] = film.actor9[0]
+    r['actor10'] = film.actor_10_string[0]
+    r['actor10_class'] = film.actor10_class[0]
+    r['actor10_code'] = film.actor10[0]
+    r['production'] = film.production[0]
+    r['distribution'] = film.distribution[0]
+    r['genre'] = film.genre[0]
+    r['rating'] = rating
+    return r
+    
 def get_celeb_class(pop):
     if pop <=2000:
         return 'A-list'
@@ -372,9 +525,19 @@ def get_act_pop(code, fake_pop):
         return fake_pop + randint(0,1000)
     
 def get_actor_name(key):
-    #print(key)
+    if key == '':
+        return ''
     if isinstance(key, float):
         return key
     row = actor_key.loc[actor_key['actor'] == key].index[0]
     #print(type(actor_key.iloc[row]['name']), actor_key.iloc[row]['name'])
     return actor_key.iloc[row]['name']
+
+def get_actor_key(name):
+    try:
+        if name == '':
+            return ''
+        row = actor_key.loc[actor_key['name'] == name].index[0]
+        return(actor_key.iloc[row]['actor'])
+    except:
+        return ''
